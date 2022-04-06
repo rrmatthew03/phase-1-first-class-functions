0848f5caf4fc794ba8cb2876e8292a877e24c35b
ryan rivera <rrmatthew03@yahoo.com>
2022-04-06 09:12:03 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+const spy = chai.spy
+
+function receivesAFunction(spy){  
+    console.log(spy)
+}
+receivesAFunction(spy)
+
+function returnsAnamedFunction(){
+
+}
+returnsAnamedFunction()
+
+
+function returnsAnAnonymousFunction(){
+
+}
